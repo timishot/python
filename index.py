724d04e82587a916ece3dfc53549f9ab6e23d88c
Timilehin Adeniran <timidotcom58@gmail.com>
2023-03-10 12:41:35 +0100

    learning pt=ython

---
+++ index.py
@@ -0,0 +1,11 @@
+money = input("How much to invest : ")
+interest_rate =  input("Interest rate : ")
+
+money = float(money)
+
+interest_rate = float(interest_rate) * .01
+
+for i in range(10):
+    money = money + (money * interest_rate)
+
+print("investment after 10 years : {:2f}".format(money))
