fd3c219f79b59d16a094c2acf5be9d92798011e9
Timilehin Adeniran <timidotcom58@gmail.com>
2023-03-29 22:52:30 +0100

    learnig about class and getter in python

---
+++ getters.py
@@ -0,0 +1,45 @@
+class Square:
+    def __init__(self, height="0", width="0"):
+        self.height = height
+        self.width= width
+    @property
+    def height(self):
+        print("Retrieveing the Height")
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if value.isdigit():
+            self.__height = value
+        else:
+            print("Please only enter numbers for height")
+
+    @property
+    def width(self):
+        print("Retrieveing the Height")
+        return self.__width
+    @width.setter
+    def width(self, value):
+
+        if value.isdigit():
+            self.__width = value
+        else:
+            print("Please only enter numbers for width")
+
+    def getArea(self):
+        return int(self.__width) * int(self.__height)
+    
+def main():
+    aSquare = Square()
+
+    height = input("Enter Height : ")
+    width = input("ENter Width : ")
+
+    aSquare.height = height
+    aSquare.width = width
+    print("Height :", aSquare.height)
+    print("Width :", aSquare.width)
+
+    print("THe Area is :", aSquare.getArea())
+
+
+main()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,1 @@
-class DogNameError(Exception):
-
-    def __init__(self, args, **kwargs):
-        Exception.__init__(self, *args,**kwargs)
-
-try:
-    DogName = input("what is your dog name :")
-
-    if any(char.isdigit() for char in DogName):
-
-        raise DogNameError
-    
-
-except DogNameError:
-    print("YOur dog name cant contain a number")
+        return self.__height
